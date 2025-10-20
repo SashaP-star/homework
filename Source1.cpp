d2f2e484c7dafbfdfb6f23b7add86e02522e87bf
SashaP-star <sashafrik17@gmail.com>
2025-10-20 17:55:55 +0300

    Add files via upload

--- Source1.cpp
+++ Source1.cpp
@@ -1,136 +1,80 @@
 #include <iostream>
-#include <fstream>
+#include <string>
+#include <windows.h> 
+#include <limits> // Для numeric_limits
+
 using namespace std;
-struct Student
-{
-	string name;
-	string supname;
-	int age;
 
+int getCurrentYear();
 
+class Book {
+private:
+    string title;
+    string author;
+    int year;
 
-};
-const int SIZE_OF_STUDENTS = 10;
+public:
+    // Конструктор с параметрами
+    Book(string t, string a, int y) {
+        title = t;
+        author = a;
+        year = y;
+    }
 
+    // Конструктор по умолчанию
+    Book() {
+        title = "empty";
+        author = "empty";
+        year = 0;
+    }
 
+    void inputAndPrint() {
+        cin.ignore(256, '\n'); // Очистка буфера перед getline
+        cout << "Enter title: ";
+        getline(cin, title);
 
+        cout << "Enter author: ";
+        getline(cin, author);
 
-int main()
-{
+        cout << "Enter year: ";
+        cin >> year;
+        
 
-	Student arr[SIZE_OF_STUDENTS];
-	int number_of_students;
-	cout << "Put much of students(no more " << SIZE_OF_STUDENTS << ")=";
-	showMenu(arr, number_of_students);
-		return 0;
-
-
-}
-void writefilestudents()
-{
-
-}
-void InputOneStudent(Student* arr_of_students, int position)
-{
-	if (position < SIZE_OF_STUDENTS)
-	{
-		cout << "Name:";
-		cin >> arr_of_students[position].name;
-		cout << " 2nd Name";
-		cin >> arr_of_students[position].supname;
-		cout << "Age";
-		cin >> arr_of_students[position].age;
-
-
-	}
-
-}
-void addstudent(Student* arr, int number_of_students)
-{
-	if (number_of_students >= SIZE_OF_STUDENTS)
-	{
-
-		cout << "massive is full" << endl;
-		return;
-	}
-	cout << "=== ADD NEW STUDENT ===" << endl;
-
-
-}
-void inputStudents(Student* arr, int number_of_students)
-{
-	for (int i = 0; i < number_of_students; i++)
-	{
-		cout << "name =";
-		cin >> arr[i].name;
-
-	}
-
-}
-void changeStudent(Student* arr, int number_of_students)
-{
-
-}
-void Showstudents(Student* arr, int number_of_students)
-{
-	cout << "students are:" << arr[SIZE_OF_STUDENTS].name;
-}
-void deletestudents(Student* arr, int number_of_students)
-{
-	for (int i = 0; i < number_of_students; i--)
-	{
-		cout << "name =";
-		cin >> arr[i].name;
-
-	}
-}
-void showMenu(Student* arr, int number_of_students)
-
-{
-
-
-	bool flag = true;
-	while (flag)
-	{
-		cout << "\n=== MENU ===\n";
-		cout << "1  - put students" << endl;
-		cout << "2  - relo" << endl;
-		cout << "3  - delete student" << endl;
-		cout << "4  - add student" << endl;
-		cout << "5  - put all students on visible" << endl;
-		
-		cout << "0  - Exit" << endl;
-		
-		
-		
-	}
-	int item;
-	cin >> item;
-
-	
-
-	switch (item)
-	{
-	case 1: inputStudents();
-		break;
-	case 2: Showstudents();
-		
-		break;
-	case 3: 
-		changeStudent();
-		break;
-	case 4: deletestudents();
-		break;
-	
-
-
-	default: cout << "Invalid choice!" << endl;
+        if (cin.fail()) {
+            cout << "Invalid input for year." << endl;
+            cin.clear();
+            cin.ignore( 256, '\n');
+            return;
         }
 
+        if (year <= 1975) {
+            cout << "Book is too old" << endl;
+        }
+        else {
+            cout << "Title = " << title << endl;
+            cout << "Author = " << author << endl;
+            cout << "Year = " << year << endl;
+        }
+    }
+};
 
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
-	
+    Book book1;
+    book1.inputAndPrint();
 
+    Book book2;
+    book2.inputAndPrint();
 
+    cout << "Current year is: " << getCurrentYear() << endl;
 
+    return 0;
+}
 
+int getCurrentYear() {
+    SYSTEMTIME tm;
+    GetLocalTime(&tm);
+    return tm.wYear;
+}
